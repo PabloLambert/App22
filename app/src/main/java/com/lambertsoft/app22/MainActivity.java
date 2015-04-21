eed2564fa7cdbac4ec06e39007e6372768863b9f
Pablo Lambert <pablo.lambert@gmail.com>
2015-04-20 22:48:26 -0300

    New release.-

--- app/src/main/java/com/lambertsoft/app22/MainActivity.java
+++ app/src/main/java/com/lambertsoft/app22/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener {
@@ -53,6 +54,11 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == R.id.action_search){
+            Toast.makeText(getApplicationContext(), "Search action is selected!", Toast.LENGTH_SHORT).show();
             return true;
         }
 
